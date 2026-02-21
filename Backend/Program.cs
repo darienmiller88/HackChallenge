de1ffea61082afa1214c53d9b66157be2c76ffd8
unknown <darienmiller88@yahoo.com>
2026-02-21 15:35:30 -0500

    feat: loading .env

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,18 +1,13 @@
+
+
+//Load .env as early as possible
+DotNetEnv.Env.Load();
+
+Console.WriteLine("Started!");
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseOpenApi();
-    app.UseSwaggerUi(config =>
-    {
-        config.DocumentTitle = "TodoAPI";
-        config.Path = "/swagger";
-        config.DocumentPath = "/swagger/{documentName}/swagger.json";
-        config.DocExpansion = "list";
-    });
-}
-
 
 app.MapGet("/", () => "Hello World!");
 
--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d48b1fd2a7782c91405c18967ea945ad5f901785")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6043913e47c2d48274f2e3ae17039d28fe47901")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
