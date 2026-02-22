eec47e945d591316d14bec607a1d3921c1a3057e
unknown <darienmiller88@yahoo.com>
2026-02-21 19:38:08 -0500

    feat: ADDED REPOS

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,5 +1,7 @@
+using System.Data;
 using System.Diagnostics;
 using api.v1.Routes;
+using Npgsql;
 
 //Load .env as early as possible
 DotNetEnv.Env.Load();
@@ -7,6 +9,21 @@ DotNetEnv.Env.Load();
 Console.WriteLine("Started!");
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IDbConnection>(sp =>
+{
+    // Get the environment variable
+    var connString = Environment.GetEnvironmentVariable("SUPA_BASE_URI");
+
+    // Throw early if missing
+    if (string.IsNullOrWhiteSpace(connString))
+    {
+        throw new InvalidOperationException("Environment variable 'SUPA_BASE_URI' is not set.");
+    }
+
+    // Return a new PostgreSQL connection
+    return new NpgsqlConnection(connString);
+});
 
 var app = builder.Build();
 
